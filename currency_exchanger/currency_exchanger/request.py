362ebd3b8868db5f5282eb87e9e483b231a11515
Mottshi <mxttshi@gmail.com>
2022-07-17 19:06:42 +0300

    Added a new request for ETH

--- currency_exchanger/currency_exchanger/request.py
+++ currency_exchanger/currency_exchanger/request.py
@@ -1,10 +1,18 @@
 import requests
 import secrets
 
+
+
 def request(API_TOKEN, BASE_URL):
   payload = {}
   headers = {
     "apikey": API_TOKEN
   }
-  response = requests.request("GET", BASE_URL, headers=headers, data = payload)
+  response_eth = requests.get(secrets.URL_FOR_ETH).json()
+  response = requests.request("GET", BASE_URL, headers=headers, data = payload).json()
+  response['rates']['ETH'] = response_eth['result']['ethusd']
   return response
+
+
+
+
