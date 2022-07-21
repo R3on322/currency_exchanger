63a6acf82e3fe5f483fc257fab9d5fc3724a2e79
R3on322 <kindeer123@mail.ru>
2022-07-21 17:42:08 +0300

    Made serializer and displayed the currency exchange rates with date

--- currency_exchanger/currency_exchanger/exchanger_api.py
+++ currency_exchanger/currency_exchanger/exchanger_api.py
@@ -1,8 +1,8 @@
-from currency_exchanger.request import request
-import secrets
+from .request import request
+from .secrets import API_TOKEN, BASE_CURRENCY_URL, URL_FOR_ETH
 
 
-request_result = request(secrets.API_TOKEN, secrets.BASE_CURRENCY_URL)
+request_result = request(API_TOKEN, BASE_CURRENCY_URL, URL_FOR_ETH)
 rates_names = ' '.join([i for i in request_result['rates'].keys()])
 
 class Exchanger:
@@ -16,7 +16,7 @@ class Exchanger:
         self.res_exch = self.exchanger()
 
     def exchanger(self) -> float:
-        self.res_exch = (self.def_val * self.num_currency) * self.count
+        self.res_exch = round((self.def_val / self.num_currency), 7) * self.count
         return self.res_exch
 
     def __repr__(self):
@@ -37,6 +37,5 @@ while True:
 
     except NameError:
         print(f'Error! Chek name of currency({rates_names})')
-
-exchngr = Exchanger(count, currency)
-print(exchngr)
+# exchngr = Exchanger(5, "ETH")
+# print(exchngr)
--- currency_exchanger/currency_exchanger/views.py
+++ currency_exchanger/currency_exchanger/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from currency_exchanger.request import request as request_currency
+from .models import Currency
+from .secrets import API_TOKEN, BASE_CURRENCY_URL
+from .serializers import CurrencySerializer
+# from .exchanger_api import Exchanger
 
-# Create your views here.
+# data_base = {'success': True, 'timestamp': 1658320863, 'base': 'USD', 'date': '2022-07-20',
+#              'rates': {'BTC': 4.2338501e-05, 'EUR': 0.980695, 'USD': 1, 'BRL': 5.441598, 'ETH': 1589.04}}
+
+
+class CurrencyView(APIView):
+
+    def get(self, request):
+        queryset = Currency.objects.all()
+        return Response({'Currencies': CurrencySerializer(queryset, many=True).data})
+
+    # def post(self, request):
+    #     pass
