d2653990c1153853a309a563a6d98bcd81f2868d
Mottshi <mxttshi@gmail.com>
2022-07-26 23:46:20 +0300

    Created a database

--- currency_exchanger/currency_exchanger/models.py
+++ currency_exchanger/currency_exchanger/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Currency(models.Model):
 
-    success = models.BooleanField()
-    def_currency = models.CharField(max_length=5)
-    last_upd_date = models.CharField(max_length=10)
-    currency_values = models.CharField(max_length=255)
+    CurrencyId = models.AutoField(primary_key=True)
+    DefCurrency = models.CharField(max_length=500)
+    LastUpdDate = models.CharField(max_length=500)
+    CurrencyValues = models.CharField(max_length=500)
 
     # {'success': True, 'timestamp': 1658314144, 'base': 'USD', 'date': '2022-07-20',
     #  'rates': {'BTC': 4.2305139e-05, 'EUR': 0.982045, 'USD': 1, 'BRL': 5.414199, 'ETH': 1579.43}}
