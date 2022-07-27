41ada7cd8dee3394d44211bf761ceb8506d901d0
R3on322 <kindeer123@mail.ru>
2022-07-27 15:50:44 +0300

    Update exchanger for new db Postgresql

--- currency_exchanger/currency_exchanger/serializers.py
+++ currency_exchanger/currency_exchanger/serializers.py
@@ -6,7 +6,7 @@ class CurrencySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Currency
-        fields = ['last_upd_date', 'currency_values']
+        fields = ['LastUpdDate', 'CurrencyValues']
 
 class ExchangerModel:
     def __init__(self, count_cur, result_cur):
@@ -17,8 +17,3 @@ class ExchangerSerializer(serializers.Serializer):
     count_cur = serializers.IntegerField(max_value=5)
     result_cur = serializers.CharField(max_length=10)
 
-#  "sourceValue" : 123.45,
-#  "resultValue" : 126919856183,
-#  "sourceCurrency" : "BTC",
-#  "resultCurrency" : "EUR"
-# }
